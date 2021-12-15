fc7d8d2556dfdbff5770b5077a7be610e3e05be1
Jerez S <>
2021-12-15 15:45:19 -0800

    added skeleton for authentication, stacks and screens

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import tw from 'tailwind-rn';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
-    <View style={tw('flex-1 justify-center items-center')}>
-      <Text>Test!</Text>
-      <Button title='Click me' />
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+
+    </NavigationContainer>
   );
 }
 
